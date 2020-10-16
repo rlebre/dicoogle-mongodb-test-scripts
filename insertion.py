13f67230ee6e9e176f3f1be51b9658c6a8f600b3
Rui Lebre <ruilebre@ua.pt>
2020-10-16 20:32:06 +0100

    implemented json insert

--- insertion.py
+++ insertion.py
@@ -7,7 +7,7 @@ from pydicom._dicom_dict import DicomDictionary as dcm_dict
 
 import time
 from pymongo import MongoClient
-from generator import generate_dicom_files
+from generator import generate_dicom_files, generate_dicom_to_json
 from local_settings import *
 
 from os import listdir
@@ -54,6 +54,19 @@ def insert_objects(mongo_connection, dicom_paths):
 
     return { 'elapsed': deltaT/1e6, 'count':len(result.inserted_ids) }
 
+def insert_json_objects(mongo_connection, dicom_paths):
+    db = mongo_connection.DicoogleDatabase
+    collection = db.DicoogleObjs
+
+    for dicom in dicom_paths:
+        dicom_json_objects.append(dicom)
+
+    deltaT = time.time_ns()
+    result = collection.insert_many(dicom_json_objects)
+    deltaT = time.time_ns() - deltaT
+
+    return { 'elapsed': deltaT/1e6, 'count':len(result.inserted_ids) }
+
 def insert_objects_by_chuncks(mongo_connection, dicom_json_objects, chunk_size=5000):
     dicom_objects_chunk = []
     results = { 'elapsed': 0.0, 'count': 0 }
@@ -74,10 +87,30 @@ def insert_objects_by_chuncks(mongo_connection, dicom_json_objects, chunk_size=5
 
     return results
 
+def insert_json_objects_by_chuncks(mongo_connection, dicom_json_objects, chunk_size=5000):
+    dicom_objects_chunk = []
+    results = { 'elapsed': 0.0, 'count': 0 }
+
+    for idx, dicom_json_obj in enumerate(dicom_json_objects):
+        dicom_objects_chunk.append(dicom_json_obj)
+
+        if len(dicom_objects_chunk) % chunk_size == 0:
+            statistics = insert_json_objects(mongo_connection, dicom_objects_chunk)
+            results['elapsed'] += statistics['elapsed']
+            results['count'] += statistics['count']
+            dicom_objects_chunk = []
+        
+    if len(dicom_objects_chunk) < chunk_size and len(dicom_objects_chunk) > 0:
+        statistics = insert_json_objects(mongo_connection, dicom_objects_chunk)
+        results['elapsed'] += statistics['elapsed']
+        results['count'] += statistics['count']
+
+    return results
 
 def main(argv):
     output_dir = 'dataset'
     number_of_files = 0
+    generate_in_dicom = True
     mongo_connection = MongoClient(MONGO_HOST, MONGO_PORT, username=MONGO_USER, password=MONGO_PASSWORD)
     #mongo_connection = MongoClient('localhost', 27017)
     chunk_size = 0
@@ -97,6 +130,11 @@ def main(argv):
                 number_of_files = int(arg)
             except ValueError:
                 sys.exit("-g | --generate argument must be integer. Program will not run.")
+        elif opt in ("-j", "--json"):
+            try:
+                generate_in_dicom = False
+            except ValueError:
+                sys.exit("-j | --json argument must be integer. Program will not run.")
         elif opt in ("-o", "--output"):
             output_dir = arg
         elif opt in ("-c", "--chunk"):
@@ -105,16 +143,22 @@ def main(argv):
             except ValueError:
                 sys.exit("-c | --chunk argument must be integer. Program will not run.")
 
-    if number_of_files > 0:
+    if number_of_files > 0 and generate_in_dicom = True:
         generate_dicom_files(output_dir, number_of_files)
+    elif number_of_files > 0 and generate_in_dicom = False:
+        generate_dicom_to_json(output_dir, number_of_files)
     
     dataset_file_list = get_dicom_files_list(output_dir)
     #dicom_json_objects = dicom2json(dataset_file_list)
 
-    if chunk_size == 0:
+    if chunk_size == 0 and generate_in_dicom = True:
         result = insert_objects(mongo_connection, dataset_file_list)
-    elif chunk_size > 0:
+    elif chunk_size > 0 and generate_in_dicom = True:
         result = insert_objects_by_chuncks(mongo_connection, dataset_file_list, chunk_size)
+    elif chunk_size == 0 and generate_in_dicom = False:
+        result = insert_json_objects(mongo_connection, dataset_file_list)
+    elif chunk_size > 0 and generate_in_dicom = False:
+        result = insert_json_objects_by_chuncks(mongo_connection, dataset_file_list, chunk_size)
 
 
     print("Inserted %d objects in %.2f milisseconds.\n" % (result['count'], result['elapsed']))
