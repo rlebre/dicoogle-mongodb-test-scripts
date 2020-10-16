95b7a68670596e71852ad7457db585d212ef03fb
Rui Lebre <ruilebre@ua.pt>
2020-10-16 10:24:45 +0100

    Added batch insertion

--- insertion.py
+++ insertion.py
@@ -54,16 +54,36 @@ def insert_objects(mongo_connection, dicom_json_objects):
 
     return { 'elapsed': deltaT/1e6, 'count':len(result.inserted_ids) }
 
+def insert_objects_by_chuncks(mongo_connection, dicom_json_objects, chunk_size=5000):
+    dicom_objects_chunk = []
+    results = { 'elapsed': 0.0, 'count': 0 }
+
+    for idx, dicom_json_obj in enumerate(dicom_json_objects):
+        dicom_objects_chunk.append(dicom_json_obj)
+
+        if len(dicom_objects_chunk) % chunk_size == 0:
+            statistics = insert_objects(mongo_connection, dicom_objects_chunk)
+            results['elapsed'] += statistics['elapsed']
+            results['count'] += statistics['count']
+            dicom_objects_chunk = []
+        
+    if len(dicom_objects_chunk) < chunk_size and len(dicom_objects_chunk) > 0:
+        statistics = insert_objects(mongo_connection, dicom_objects_chunk)
+        results['elapsed'] += statistics['elapsed']
+        results['count'] += statistics['count']
+
+    return results
 
 
 def main(argv):
     output_dir = 'dataset'
     number_of_files = 0
-    mongo = MongoClient(MONGO_HOST, MONGO_PORT, username=MONGO_USER, password=MONGO_PASSWORD)
-    #mongo = MongoClient('localhost', 27017)
+    #mongo_connection = MongoClient(MONGO_HOST, MONGO_PORT, username=MONGO_USER, password=MONGO_PASSWORD)
+    mongo_connection = MongoClient('localhost', 27017)
+    chunk_size = 0
 
     try:
-        opts, args = getopt.getopt(argv,"hg:o:",["ifile=","output="])
+        opts, args = getopt.getopt(argv,"hg:o:c:",["ifile=","output="])
     except getopt.GetoptError:
         print('insert.py -i <number of DICOM files to generate> -o <output_directory>')
         sys.exit(2)
@@ -79,13 +99,23 @@ def main(argv):
                 sys.exit("-g | --generate argument must be integer. Program will not run.")
         elif opt in ("-o", "--output"):
             output_dir = arg
+        elif opt in ("-c", "--chunk"):
+            try:
+                chunk_size = int(arg)
+            except ValueError:
+                sys.exit("-c | --chunk argument must be integer. Program will not run.")
 
     if number_of_files > 0:
         generate_dicom_files(output_dir, number_of_files)
     
     dataset_file_list = get_dicom_files_list(output_dir)
     dicom_json_objects = dicom2json(dataset_file_list)
-    result = insert_objects(mongo, dicom_json_objects)
+
+    if chunk_size == 0:
+        result = insert_objects(mongo_connection, dicom_json_objects)
+    elif chunk_size > 0:
+        result = insert_objects_by_chuncks(mongo_connection, dicom_json_objects, chunk_size)
+
 
     print("Inserted %d objects in %.2f milisseconds.\n" % (result['count'], result['elapsed']))
 
